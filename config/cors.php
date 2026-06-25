793b373c268f37095ea79cc1b2d91fc38983e1ef
Ato <ato@gmail.com>
2026-06-25 11:24:20 +0530

    Fix CORS

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://exquisite-beijinho-8d9b34.netlify.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
